13f985d7d5ee227e6eb64df2e978d810834dc24b
hammer <34701167+Hammer0724@users.noreply.github.com>
2021-07-01 18:20:48 +0900

    first commit

---
+++ src/vc/beginners_selection/Abc049C.java
@@ -0,0 +1,11 @@
+package vc.beginners_selection;
+
+import java.util.Scanner;
+
+public class Abc049C {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+    }
+}
